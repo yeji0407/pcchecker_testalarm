9f1bd4a6e589b0e104bfd1c8db3ae46b2a4a65ca
yeji0407 <yejisoft@gmail.com>
2020-10-05 20:10:26 +0900

    notify success

--- debian/usr/bin/test_on_calendar.sh
+++ debian/usr/bin/test_on_calendar.sh
@@ -4,5 +4,5 @@
 #python3 /home/yeji/PycharmProjects/pythonProject4/main.py  >> /tmp/OnCalendarLog.txt
 #python3 /home/yeji/PycharmProjects/pythonProject5/main.py
 #python3 /home/yeji/git_pcchecker/hamonikr-pcchecker/debian/usr/bin/alarm.py
-python3 /usr/bin/pcchecker
+python3 /usr/bin/notify
 
